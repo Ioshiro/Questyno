65c7a8b406be3b80741a5a84b422b8145390ddce
MrsKugla <benedetta.scattolin988@gmail.com>
2024-01-19 21:50:58 +0100

    fix definitivo susan lee

--- media/lua/client/npcyni.lua
+++ media/lua/client/npcyni.lua
@@ -232,8 +232,8 @@ table.insert(SFQuest_Database.WorldPool, {
 SFQuest_Database.MannequinPool["10822x9072x0"] = {
     sprite = "location_shop_mall_01_65",
     direction = "E",
-    hair = "PulledBack",
-    haircolor = "0.1,0.05,0.05",
+    hair = "LongBraids",
+    haircolor = "0.8,0.6,0.0",
     beard = "",
     outfit = "SusanLee"
 }
