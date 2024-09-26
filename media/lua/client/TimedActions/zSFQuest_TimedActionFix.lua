d8a570ef9bf4f17c33fed57eb562753d57ffcee5
Stefano Scalfari <vorshim92@gmail.com>
2024-09-27 01:05:59 +0200

    additemOverWrite still not working properly

---
+++ media/lua/client/TimedActions/zSFQuest_TimedActionFix.lua
@@ -0,0 +1,23 @@
+require "TimedActions/ISCraftAction"
+
+local untouched = ISCraftAction.perform;
+function ISCraftAction:perform()
+    untouched(self);
+    SF_MissionPanel.instance:checkQuestForCompletionByType("item", nil, "Obtained");
+    print("ISCraftAction Successful overwrite");
+end
+
+-- local oldAdditem = SF_MissionPanel.Commands.additem
+function additemOverWrite(item, quantity)
+    local inv = getPlayer():getInventory();
+	inv:AddItems(item, quantity);
+    SF_MissionPanel.instance:checkQuestForCompletionByType("item", nil, "Obtained");
+    print("additem Successful overwrite");
+end
+
+
+Events.OnGameStart.Add(
+    function()
+        SF_MissionPanel.Commands["additem"] = additemOverWrite;
+    end
+) 
