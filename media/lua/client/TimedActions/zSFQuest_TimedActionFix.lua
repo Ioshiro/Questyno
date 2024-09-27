3e060dd953d55c05da1304d4e3625208b55857d8
Stefano Scalfari <vorshim92@gmail.com>
2024-09-27 09:36:50 +0200

    real fix for additem

--- media/lua/client/TimedActions/zSFQuest_TimedActionFix.lua
+++ media/lua/client/TimedActions/zSFQuest_TimedActionFix.lua
@@ -7,12 +7,27 @@ function ISCraftAction:perform()
     print("ISCraftAction Successful overwrite");
 end
 
+local function postAddItem()
+    SF_MissionPanel.instance:checkQuestForCompletionByType("item", nil, "Obtained");
+    -- print("additem Successful overwrite again");
+end
+
+local tickAdditem = 20
+local function delayAddItem()
+    if tickAdditem <= 0 then
+        Events.OnTick.Remove(delayAddItem)
+        postAddItem()
+    end
+    tickAdditem = tickAdditem - 1
+end
+
 -- local oldAdditem = SF_MissionPanel.Commands.additem
-function additemOverWrite(item, quantity)
+local function additemOverWrite(item, quantity)
     local inv = getPlayer():getInventory();
 	inv:AddItems(item, quantity);
-    SF_MissionPanel.instance:checkQuestForCompletionByType("item", nil, "Obtained");
-    print("additem Successful overwrite");
+    Events.OnTick.Add(delayAddItem)
+    -- SF_MissionPanel.instance:checkQuestForCompletionByType("item", nil, "Obtained"); --arriva troppo presto.
+    -- print("additem Successful overwrite");
 end
 
 
