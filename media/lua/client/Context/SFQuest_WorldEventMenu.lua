204997d3fa2a0059b11680dbb7199271f737338d
Stefano Scalfari <vorshim92@gmail.com>
2025-02-05 00:00:39 +0100

    test new npc acquire

--- media/lua/client/Context/SFQuest_WorldEventMenu.lua
+++ media/lua/client/Context/SFQuest_WorldEventMenu.lua
@@ -4,35 +4,34 @@ SFQuest_WorldEventMenu = function(player, context, worldobjects, test)
 	if test then return ISWorldObjectContextMenu.setTest() end
 	if not playerObj:getModData().missionProgress.WorldEvent then return end
 
-	local square;
-	
-	for i,v in ipairs(worldobjects) do
-		square = v:getSquare();
-		break;
-	end
+	local square = worldobjects[1]:getSquare();
 
-	local x,y,z = tostring(square:getX()), tostring(square:getY()), tostring(square:getZ());
-	local sqTag = x .. "x" .. y .. "x" .. z;
-	--print("Clicked square was " .. sqTag);
-	if playerObj:getModData().missionProgress.WorldEvent[sqTag] then
-		local event = playerObj:getModData().missionProgress.WorldEvent[sqTag];
-		local worldinfo = SF_MissionPanel.instance:getWorldInfo(event.identity);
-		local dialogueinfo = SF_MissionPanel.instance:getDialogueInfo(event.dialoguecode);
-		local questid = event.quest;
-		local name = getText(worldinfo.name);
-		local faction = worldinfo.faction;
-		local tier;
-		if faction then
-			tier = SF_MissionPanel.instance:getReputationTier(faction, playerObj);
-		end
-		if tier and worldinfo.tiers and worldinfo.tiers[tier] and worldinfo.tiers[tier].name then
-			name = getText(worldinfo.tiers[tier].name);
-		end
-		local contextWords = getText(dialogueinfo.context, name);
-
-        	local worldOption = context:addOptionOnTop(contextWords, worldobjects, onInteraction2, playerObj, square, worldinfo, dialogueinfo, questid);
-			worldOption.iconTexture = getTexture(worldinfo.picture);
-	end
+	local startingX,startingY,startingZ = tostring(square:getX()), tostring(square:getY()), tostring(square:getZ());
+	local x1, y1, x2, y2 = startingX-1, startingY-1, startingX+1, startingY+1
+	for i = x1, x2 do
+        for j = y1, y2 do
+            local sqTag = i.."x"..j.."x"..startingZ;
+			if playerObj:getModData().missionProgress.WorldEvent[sqTag] then
+				local event = playerObj:getModData().missionProgress.WorldEvent[sqTag];
+				local worldinfo = SF_MissionPanel.instance:getWorldInfo(event.identity);
+				local dialogueinfo = SF_MissionPanel.instance:getDialogueInfo(event.dialoguecode);
+				local questid = event.quest;
+				local name = getText(worldinfo.name);
+				local faction = worldinfo.faction;
+				local tier;
+				if faction then
+					tier = SF_MissionPanel.instance:getReputationTier(faction, playerObj);
+				end
+				if tier and worldinfo.tiers and worldinfo.tiers[tier] and worldinfo.tiers[tier].name then
+					name = getText(worldinfo.tiers[tier].name);
+				end
+				local contextWords = getText(dialogueinfo.context, name);
+		
+					local worldOption = context:addOptionOnTop(contextWords, worldobjects, onInteraction2, playerObj, square, worldinfo, dialogueinfo, questid);
+					worldOption.iconTexture = getTexture(worldinfo.picture);
+			end
+        end
+    end
 end
 
 onInteraction2 = function(worldobjects, playerObj, square, worldinfo, dialogueinfo, questid)
@@ -42,3 +41,15 @@ onInteraction2 = function(worldobjects, playerObj, square, worldinfo, dialoguein
 end
 
 Events.OnFillWorldObjectContextMenu.Add(SFQuest_WorldEventMenu);
+
+
+function ISDupingPlayer.addCoords(startingX, startingY)
+    ISDupingPlayer.coords = {}
+    local x1, y1, x2, y2 = startingX-1, startingY-1, startingX+1, startingY+1
+    for i = x1, x2 do
+        for j = y1, y2 do
+            if Debug then print("ISDupingPlayer: Adding coords: " .. i .. " " .. j) end
+            ISDupingPlayer.coords[i.."_"..j] = {x = i, y = j}
+        end
+    end
+end
