d22b9991470fd2fee664a32c27cbfba7c1dd6693
pynopyno <magnanimog@gmail.com>
2025-02-19 22:38:24 +0100

    create tag armi oro random

--- media/lua/client/questyno_rewardPools.lua
+++ media/lua/client/questyno_rewardPools.lua
@@ -8,3 +8,26 @@ SFQuest_Database.RandomRewardItemPool.SleepingBags = {
     "SleepingBag_Camo_Packed;1",
     "SleepingBag_Spiffo_Packed;1",
 }
+SFQuest_Database.RandomRewardItemPool.GoldWeapons1 = {
+    "SOMWReward.SRewardKnife;1",
+    "SOMWReward.SRewardHammer;1",
+    "SOMWReward.SRewardCleaver;1",
+}
+
+SFQuest_Database.RandomRewardItemPool.GoldWeapons2 = {
+    "SOMWReward.SRewardKnife;1",
+    "SOMWReward.SRewardHammer;1",
+    "SOMWReward.SRewardCleaver;1",
+    "SOMWReward.SRewardHandAxe;2",
+    "SOMWReward.SRewardBaseballBat;2",
+    "SOMWReward.SRewardSpear;2",
+}
+SFQuest_Database.RandomRewardItemPool.GoldWeapons3 = {
+
+    "SOMWReward.SRewardHandAxe;1",
+    "SOMWReward.SRewardBaseballBat;1",
+    "SOMWReward.SRewardSpear;1",
+    "SOMWReward.SRewardAxe;2",
+    "SOMWReward.SRewardMachete;2",
+    "SOMWReward.SRewardKatana;2",
+}
