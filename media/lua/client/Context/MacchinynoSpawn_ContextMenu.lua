81c8456d98f5f636c78391e7980931c57cbd9343
Ioshiro <golyshev@novabase.it>
2025-03-24 15:43:12 +0100

    add hotwire to spawned vehicle

--- media/lua/client/Context/MacchinynoSpawn_ContextMenu.lua
+++ media/lua/client/Context/MacchinynoSpawn_ContextMenu.lua
@@ -51,6 +51,7 @@ function ISInventoryMenuElements.ContextBikeKey()
 			else
 				local vehicle = addVehicleDebug(_v, _p:getDir(), nil, _p:getSquare())
                 vehicle:repair()
+                vehicle:setHotwired(true)
 			end
             _p:getInventory():Remove(key)
 	end
