79a456c4a766a20254680ca96e957d8190f94568
pynopyno <magnanimog@gmail.com>
2025-06-06 19:40:19 +0200

    cambiata quest di vivian con 8 torce scariche

--- media/lua/client/questyni_yearly.lua
+++ media/lua/client/questyni_yearly.lua
@@ -537,7 +537,7 @@ table.insert(SFQuest_Database.QuestPool,{
 
     awardsrep = "LaResistenza;200", -- REP REWARD
     awardsitem = "TW.LargePropaneTank;1",
-    needsitem = "PredicateDrainable#BlowTorch;10;0.1", -- REQUIRE
+    needsitem = "PredicateDrainable#BlowTorch;8;0.1", -- REQUIRE
     completesound = "levelup",
     dailycode = "Questyno_VivianParker",
     lore = { "IGUI_SFQuest_Questyno_VivianParker1_Lore" },
